a218cf53f48ac190c9c71abc110bb2a7c325ffce
mumira-bit <mumiraesther41@gmail.com>
2025-06-06 00:43:28 +0300

    functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+// 1. Function declaration with default value
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+// 2. Function expression with default value
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+// 3. Function that returns a function, using template literal and wrapper
+function wrapAdjective(wrapper = "*") {
+  return function(adjective = "special") {
+    return `You are ${wrapper}${adjective}${wrapper}!`;
+  };
+}
+
